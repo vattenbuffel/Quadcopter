1457cd5399029aceea30931da69f593939937412
vattenbuffel <nois97.noa@gmail.com>
2020-06-22 19:45:08 +0200

    clean code

--- Quadcopter-nano/src/main.cpp
+++ Quadcopter-nano/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <Servo.h>
 #include "complementary-filter.h"
 #include "controller.h"
 
@@ -8,7 +7,7 @@
   Calibrate the motors
 */
 
-Servo ESC;
+
 
 void setup() {
   Serial.begin(115200);
@@ -17,7 +16,6 @@ void setup() {
   
   init_mpu();
   controller_init();
-  ///ESC.attach(11,1000,2000);
 
   
   
@@ -26,26 +24,20 @@ void setup() {
 
 void loop() {
    
-    update_filter();
-    /*Serial.print("X :");
-    Serial.print(radToDeg(get_X()));
-    Serial.print("  Y :");
-    Serial.print(radToDeg(get_Y()));
-    Serial.print("  Z :");
-    Serial.println(radToDeg(get_Z()));
-    delay(20);*/
+  update_filter();
+  /*Serial.print("X :");
+  Serial.print(radToDeg(get_X()));
+  Serial.print("  Y :");
+  Serial.print(radToDeg(get_Y()));
+  Serial.print("  Z :");
+  Serial.println(radToDeg(get_Z()));
+  delay(20);*/
 
-  /*for (int i = 0; i < 0; i+=100){ 
-    int val = map(i, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
-    ESC.write(val);    // Send the signal to the ESC
-    Serial.println(i);
-    delay(500);
-  }*/
 
   controller_update();
-  //ESC.writeMicroseconds(1700);
 
-  delay(20);
+
+  delay(20); // The filter and controller update seems to take almost no time at all so this should definitliy satisfy h = 0.025
 
 
 
